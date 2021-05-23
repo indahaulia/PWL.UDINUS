b76d432b70ae8ba8268419e3f6426b96c63467a6
indahaulia <indahaulia019@gmail.com>
2021-05-23 16:30:08 +0700

    update tugas

--- minggu 7/crud/index.php
+++ minggu 7/crud/index.php
@@ -1,38 +1,37 @@
 <?php 
 include 'conn.php'; 
 include 'conf.php'; 
-?> 
-<!DOCTYPE html> 
-<html> 
-<head> 
-    <title>PDO CRUDSRUD</title> 
-    <link rel="stylesheet" type="text/css" href="assets/css/css.css"> 
-</head> 
-<body> 
-    <div class="top-gray"></div> 
-    <div class="header"> 
-    <div class="big-logo">Latihan CRUDSRUD</div> 
-    <?php include 'menu.php';?> 
-    </div> <div class="content"><div class="container"> 
-    <?php 
-        if(!is_null(get('p'))){ 
-            switch (get('p')) { 
-                case 'home': inc('home'); 
-                break; 
-                case 'sekolah': switch (get('m')) { 
-                    case 'home': inc('tampilan/sekolah/all'); 
-                    break; 
-                    case 'add': inc('tampilan/sekolah/add'); 
-                    break; 
-                    case 'edit': inc('tampilan/sekolah/edit'); 
-                    break; 
-                    default: inc('tampilan/sekolah/all'); 
-                    break; 
-                } 
-                break; default: inc('home'); break; } 
-            }else{ inc('home'); } 
-    ?> 
-    </div>
-    </div> 
-</body> 
-</html>
+session_start(); 
+if (isset($_POST['Login'])) { 
+    $user = $_POST['user']; 
+    $pass = $_POST['pass']; 
+    //periksa login 
+    $addonq = " WHERE username = '".$user."' and password='".$pass."'"; 
+    $hasil = $koneksi->prepare("SELECT * FROM tb_user ".$addonq." "); 
+    $hasil->execute(); 
+    $data = $hasil->fetchAll(); 
+        if (!empty($data)) { 
+
+        //menciptakan session 
+        print_r($data);
+        $_SESSION['username'] = $user; 
+        $_SESSION['nama'] = $data[0]['nama']; 
+        $_SESSION['hak'] = $data[0]['hak']; 
+        print_r($_SESSION);
+        //menuju ke halaman pemeriksaan session 
+        header("Location: dashboard.php");
+        }
+    }else { 
+            ?> <html> 
+            <head> 
+            <title>Login here...</title> 
+            </head> 
+            <body> 
+            <form action="" method="post"> 
+            <h2>Login Here...</h2> 
+            Username : <input type="text" name="user"><br> <br>
+            Password : <input type="password" name="pass"><br> 
+            <input type="submit" name="Login" value="Log In"> </form></body></html><?php 
+            }
+        
+?>
