8dc6a25e1574f0caaba2d6349247aaa01c57ba46
indahaulia <indahaulia019@gmail.com>
2021-07-07 19:55:40 +0700

    update

---
+++ minggu 12/Latihan/crud/application/views/template/footer.php
@@ -0,0 +1,6 @@
+	
+
+		<script type="text/javascript"> var base_url = "<?php echo base_url ()?>"</script>
+		<script type="text/javascript" src="<?php echo base_url()?>assets/bootstrap/js/bootstrap.bundle.min.js"></script>
+	</body>
+</html>
